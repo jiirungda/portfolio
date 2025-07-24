8422afb1989d886bd2a07ac1a70de5fdab9ded7c
jiirungda <jii020116@gmail.com>
2025-07-24 17:44:10 +0900

    .

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -28,6 +28,23 @@ $(document).ready(function(){
 		},
 
 	}); //fullpage
+	
+	const cardnews_swiper = new Swiper('.cardnews .swiper', { /* 팝업을 감싼는 요소의 class명 */
+
+        //autoplay: {  /* 팝업 자동 실행 */
+        //    delay: 5000,
+        //    disableOnInteraction: false,
+        //},
+    
+        //effect: "fade", /* fade 효과 */
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        
+        navigation: {  /* 이전, 다음 버튼 */
+            nextEl: '.cardnews .btn_wrap button.btn_next',  /* 다음 버튼의 클래스명 */
+            prevEl: '.cardnews .btn_wrap button.btn_prev',  
+        },
+    
+    });
 
 	$(window).on('scroll mousemove', function(e){  /* html cursor가 마우스 포인터를 따라다니게 하는 값 */
 		$('.cursor').css('left', e.pageX + 'px');
