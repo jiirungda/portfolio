69310aea15ede2e689e13be77dbd1285a8f540eb
jiirungda <jii020116@gmail.com>
2026-04-13 17:48:15 +0900

    Update main.js

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -29,7 +29,7 @@ $(document).ready(function(){
 
 	}); //fullpage
 	
-	const cardnews_swiper = new Swiper('.cardnews .swiper', { /* 팝업을 감싼는 요소의 class명 */
+	const cardnews_swiper = new Swiper('.project03 .swiper', { /* 팝업을 감싼는 요소의 class명 */
 
         //autoplay: {  /* 팝업 자동 실행 */
         //    delay: 5000,
@@ -40,8 +40,8 @@ $(document).ready(function(){
         loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
         
         navigation: {  /* 이전, 다음 버튼 */
-            nextEl: '.cardnews .btn_wrap button.btn_next',  /* 다음 버튼의 클래스명 */
-            prevEl: '.cardnews .btn_wrap button.btn_prev',  
+            nextEl: '.project03 .btn_wrap button.btn_next',  /* 다음 버튼의 클래스명 */
+            prevEl: '.project03 .btn_wrap button.btn_prev',  
         },
     
     });
